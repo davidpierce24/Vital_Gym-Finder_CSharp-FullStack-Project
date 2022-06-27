1ed654354d3e2b519638d4a76edb7c4ac195d7a2
davidpierce24 <pie2ce@gmail.com>
2022-06-27 16:03:45 -0400

    updates

--- Vital/Controllers/HomeController.cs
+++ Vital/Controllers/HomeController.cs
@@ -27,6 +27,92 @@ public class HomeController : Controller
     }
 
 
+// Route to register a user ------------------------------------------
+    [HttpPost("user/register")]
+    public IActionResult RegisterUser(User newUser)
+    {
+        if(ModelState.IsValid){
+            // validation for unique email
+            if(_context.Users.Any(d => d.Email == newUser.Email)){
+                // user email already in db
+                ModelState.AddModelError("Email", "Email is already in use!");
+                return View("Index");
+            }
+            // validation for unique username
+            if(_context.Users.Any(d => d.Username == newUser.Username)){
+                // username already in db
+                ModelState.AddModelError("Username", "Username is already in use!");
+                return View("Index");
+            }
+            // password hasing
+            PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("user", newUser.UserId);
+            return RedirectToAction("Success");
+        } else {
+            return View("Index");
+        }
+    }
+
+// route to process a user login -------------------------------------------
+    [HttpPost("user/login")]
+    public IActionResult LoginUser(LogUser loginUser)
+    {
+        if(ModelState.IsValid){
+            // check to see if email is in db
+            User userInDb = _context.Users.FirstOrDefault(d => d.Email == loginUser.LogEmail);
+            if(userInDb == null){
+                // no email in database
+                ModelState.AddModelError("LogEmail", "Invalid login attempt");
+                return View("Index");
+            }
+            // check for hashed password
+            PasswordHasher<LogUser> Hasher = new PasswordHasher<LogUser>();
+            var result = Hasher.VerifyHashedPassword(loginUser, userInDb.Password, loginUser.LogPassword);
+            // if password was incorrect
+            if(result == 0){
+                ModelState.AddModelError("LogEmail", "Invalid login attempt");
+                return View("Index");
+            }
+            HttpContext.Session.SetInt32("user", userInDb.UserId);
+            return RedirectToAction("UserDashboard");
+        } else {
+            return View("Index");
+        }
+    }
+
+// route to process an owner registration -------------------------------------
+    [HttpPost("owner/register")]
+    public IActionResult RegisterOwner(Owner newOwner)
+    {
+        if(ModelState.IsValid){
+            // validation for unique email
+            if(_context.Users.Any(d => d.Email == newOwner.Email)){
+                // user email already in db
+                ModelState.AddModelError("Email", "Email is already in use!");
+                return View("Index");
+            }
+            // validation for unique username
+            if(_context.Users.Any(d => d.Username == newOwner.Username)){
+                // username already in db
+                ModelState.AddModelError("Username", "Username is already in use!");
+                return View("Index");
+            }
+            // password hasing
+            PasswordHasher<Owner> Hasher = new PasswordHasher<Owner>();
+            newOwner.Password = Hasher.HashPassword(newOwner, newOwner.Password);
+            _context.Add(newOwner);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("owner", newOwner.OwnerId);
+            return RedirectToAction("OwnerDashboard");
+        } else {
+            return View("Index");
+        }
+    }
+
+
     public IActionResult Privacy()
     {
         return View();
---
+++ Vital/Models/LogOwner.cs
@@ -0,0 +1,15 @@
+#pragma warning disable CS8618
+using System.ComponentModel.DataAnnotations;
+
+namespace Vital.Models;
+
+public class LogOwner
+{
+    [EmailAddress]
+    [Required]
+    public string LogEmail {get; set;}
+    [Required]
+    [MinLength(8)]
+    [DataType(DataType.Password)]
+    public string LogPassword {get; set;}
+}
