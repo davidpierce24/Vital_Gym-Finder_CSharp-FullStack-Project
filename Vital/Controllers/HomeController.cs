e08134b019d21098f181380ce1f3a1874adcdde9
davidpierce24 <pie2ce@gmail.com>
2022-06-27 11:55:56 -0400

    updates

--- Vital/Controllers/HomeController.cs
+++ Vital/Controllers/HomeController.cs
@@ -1,27 +1,41 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Vital.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vital.Controllers;
 
 public class HomeController : Controller
 {
+    private MyContext _context;
+    private readonly IConfiguration _config;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, MyContext context, IConfiguration config)
     {
         _logger = logger;
+        _context = context;
+        _config = config;
     }
+
 
     public IActionResult Index()
     {
         return View();
     }
 
+
     public IActionResult Privacy()
     {
         return View();
     }
+
+
+
+
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
--- Vital/Program.cs
+++ Vital/Program.cs
@@ -1,7 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Vital.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var GoogleMapsApiKey = builder.Configuration["GoogleMaps:ApiKey"];
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
+builder.Services.AddDbContext<MyContext>(options =>
+{
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
@@ -11,6 +23,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseStaticFiles();
+
+app.UseSession();
 
 app.UseRouting();
 
